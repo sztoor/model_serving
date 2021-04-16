cbee745a79d052a1a0de43ce8f3e8e026da9f613
Salman Toor <salman.toor@gmail.com>
2021-04-16 11:44:06 +0200

    Update start_instance.py

--- openstack-client/single_node_without_docker_client/start_instance.py
+++ openstack-client/single_node_without_docker_client/start_instance.py
@@ -22,7 +22,7 @@ auth = loader.load_from_options(auth_url=env['OS_AUTH_URL'],
                                 username=env['OS_USERNAME'],
                                 password=env['OS_PASSWORD'],
                                 project_name=env['OS_PROJECT_NAME'],
-                                project_domain_name=env['OS_USER_DOMAIN_NAME'],
+                                project_domain_id=env['OS_PROJECT_DOMAIN_ID'],
                                 #project_id=env['OS_PROJECT_ID'],
                                 user_domain_name=env['OS_USER_DOMAIN_NAME'])
 
